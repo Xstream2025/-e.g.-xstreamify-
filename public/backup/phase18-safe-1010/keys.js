7ac5b01cc484757be9043a4deb3aee54ee938bf9
Xstreamify <info@xstreamify.com>
2025-10-12 21:39:44 -0400

    💄 Adjust logo position and sync layout

---
+++ public/backup/phase18-safe-1010/keys.js
@@ -0,0 +1,9 @@
+// keys.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCiQrxPoH7s56dr9m0OJlbRPo9QRib0Q",
+  authDomain: "xstreamify-auth.firebaseapp.com",
+  projectId: "xstreamify-auth",
+  storageBucket: "xstreamify-auth.appspot.com",
+  messagingSenderId: "860454287241",
+  appId: "1:860454287241:web:fc3eef7bf0ddaef2cc5358"
+};
